93ec96e5dacc8e5c885bd97d2d3ede1801934019
James Alexander <j.alexanderbryant@gmail.com>
2019-06-10 23:36:43 -0400

    clean up.

--- decks/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import Deck, Card
-
-# Register your models here.
-admin.site.register(Deck)
-admin.site.register(Card)
--- decks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-  path('decks/', views.DeckList.as_view()),
-  path('decks/<int:pk>/', views.DeckDetail.as_view())
-]
